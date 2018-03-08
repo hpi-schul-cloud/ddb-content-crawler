fee0b520b2fc8c414fdf844e3871cb9fee4400e4
rauterberg <rauterberg@goldland-media.com>
2018-03-08 17:02:03 +0100

    refactored

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -4,17 +4,18 @@ from jsonschema import ValidationError
 from exceptions import MappingException
 from crawler.feed import DeutscheDigitaleBibliothekFeed, LocalJsonFeed
 from schema import ResourceSchema, ResourceAPI
-from crawler.mappings import Mapping
+from crawler.mapping import DDBSChulCloudTargetToSourceMapper, TargetToSourceMapper
 
 
 class Crawler:
     provider_name = None
-    target_to_source_mapping = None
+    target_to_source_mapper = None
     source_api = None
 
-    def __init__(self, target_api=ResourceAPI) -> None:
+    def __init__(self, target_to_source_mapper: TargetToSourceMapper, target_api=ResourceAPI) -> None:
         self.logger = logging.getLogger(self.provider_name)
         self.source_api = self.source_api()
+        self.target_to_source_mapper = target_to_source_mapper
         self.target_api = target_api()
 
     def log(self, message):
@@ -29,21 +30,7 @@ class Crawler:
                 self.target_api.add_resource(resource)
 
     def parse(self, element: dict) -> dict:
-        target_dict = {}
-        for key, transformation in self.target_to_source_mapping.items():
-            try:
-                if isinstance(transformation, Mapping):
-                    target_dict[key] = transformation.transform(key, element)
-                elif type(transformation) is str:
-                    target_dict[key] = element[transformation]
-                elif transformation is None:
-                    target_dict[key] = element[key]
-                else:
-                    raise MappingException('mapping for key{} is invalid')
-            except KeyError:
-                print('no match for key {} on element {}'.format(key, element['id'], element['title']))
-        print(target_dict)
-        return target_dict
+        return self.target_to_source_mapper.map(element)
 
     def validate(self, resource_dict):
         target_format = ResourceSchema(self.provider_name, **resource_dict)
@@ -55,36 +42,9 @@ class Crawler:
         return target_format
 
 
-base_url = "https://www.deutsche-digitale-bibliothek.de"
-
-
-def get_item_url(_, item):
-    return "{}/item/{}".format(base_url, item['id'])
-
-
-def build_url_from_key(key, item):
-    if key in item:
-        return "{}/{}".format(base_url, item[key])
-    return ''
-
-
-def get_description(_, item):
-    return "{title} {subtitle}".format(**item)
-
-
 class DeutscheDigitaleBibliothekCrawler(Crawler):
     provider_name = "Deutsche Digitale Bibliothek"
     source_api = DeutscheDigitaleBibliothekFeed
 
-    target_to_source_mapping = {
-        "title": "title",
-        "url": Mapping("id", get_item_url),
-        "originId": "id",
-        "description": Mapping("beschreibung", get_description),
-        # "licenses": None,  # "Mapping("rechte", lambda m: [w.text for w in m]),
-        "mimeType": "media",
-        # "contentCategory": None,
-        "tags": "category",  # lambda m,_: [w.strip() for w in m[0].text.split(';')]),
-        "thumbnail": Mapping("thumbnail", build_url_from_key),
-        # "providerName": None,
-    }
+    def __init__(self):
+        super().__init__(DDBSChulCloudTargetToSourceMapper())
--- src/crawler/mappings.py
+++
@@ -1,8 +0,0 @@
-def default_transform(key, item):
-    return item[key]
-
-
-class Mapping:
-    def __init__(self, name: str, transform: default_transform) -> None:
-        self.name = name
-        self.transform = transform
