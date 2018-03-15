ff787c8a1de8d7d4ff92342c258569785af0bf68
rauterberg <rauterberg@goldland-media.com>
2018-03-15 19:07:14 +0100

    default local env

--- src/crawler/feed.py
+++ src/crawler/feed.py
@@ -1,9 +1,10 @@
+from threading import Thread
+
 from abc import ABC, abstractmethod
 
 import requests
 import json
 import settings
-from exceptions import ConfigurationError
 
 
 class SourceFeed(ABC):
@@ -13,6 +14,17 @@ class SourceFeed(ABC):
     def get_feed(self) -> dict:
         ...
 
+
+def get_media_url(type_id):
+    return 'https://api.deutsche-digitale-bibliothek.de/search?query=*&facet=type_fct&type_fct=mediatype_00{}'.format(
+        type_id)
+
+def get_sparte_url(type_id):
+    return 'https://api.deutsche-digitale-bibliothek.de/search?query=*&facet=type_fct&type_fct=mediatype_00{}'.format(
+        type_id)
+
+def get_item_details(item_id):
+    return 'https://api.deutsche-digitale-bibliothek.de/{id}/UPKFBXUQ6LUL6SMYNV4JBQZN7I3CR74T/view'.format(id=item_id)
 
 class DeutscheDigitaleBibliothekFeed(SourceFeed):
     @property
@@ -24,7 +36,27 @@ class DeutscheDigitaleBibliothekFeed(SourceFeed):
     def __init__(self):
         self.page = 0
         self.max_result = 9999999999
+        self.check_sizes()
 
+    def check_sizes(self):
+        r = requests.get(self.base_url, params={'rows': 1},
+                         headers=self.headers)
+        if r.ok:
+            response_dict = r.json()
+            print('all %i' % (response_dict['numberOfResults'],))
+        acc = 0
+        for i in range( 10):
+            r = requests.get(get_media_url(i), params={'rows': 1},
+                             headers=self.headers)
+            if r.ok:
+                response_dict = r.json()
+                acc += response_dict['numberOfResults']
+                print('media_type00{} {} '.format(i, response_dict['numberOfResults']))
+                try:
+                    print(response_dict['results'][0]['docs'][0]['media'])
+                except IndexError:
+                    pass
+                print(acc)
 
     # https://api.deutsche-digitale-bibliothek.de/doku/display/ADD/search#search-Request4
     # https://api.deutsche-digitale-bibliothek.de/doku/display/ADD/Medientyp
@@ -39,9 +71,11 @@ class DeutscheDigitaleBibliothekFeed(SourceFeed):
                 print(response_dict)
                 self.max_result = response_dict['numberOfResults']
                 for item in response_dict['results'][0]['docs']:
+                    #todo schedule item ids
                     yield item
             else:
-                raise ConnectionRefusedError('The Feed could not be established: status:{} {}'.format(r.status_code, r.content))
+                raise ConnectionRefusedError(
+                    'The Feed could not be established: status:{} {}'.format(r.status_code, r.content))
 
     @property
     def headers(self):
@@ -50,6 +84,10 @@ class DeutscheDigitaleBibliothekFeed(SourceFeed):
             'Host': 'api.deutsche-digitale-bibliothek.de',
             'Authorization': 'OAuth oauth_consumer_key="{}"'.format(settings.API_KEY)
         }
+
+
+class FeedWorkerThread(Thread):
+    pass
 
 
 class LocalJsonFeed(SourceFeed):
